22c987ab6b40ea639cfa9e10296eab409a4670bf
danieliramain <danieliramain97@gmail.com>
2022-03-28 13:18:24 -0300

    mock tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        text: 'Terminar modulo Angular',
+        day: '31 Marzo 2022',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Terminar modulo bases de datos',
+        day: '33 Abril 2022',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Terminar modulo back-end',
+        day: '30 Mayo 2022',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: 'Terminar modulo devOps',
+        day: '31 Agosto 2022',
+        reminder: true
+    },
+]
